182383f0e309887a98e3dda2c52d17f2a1379a54
IO <alfamastudio11@gmail.com>
2025-02-21 14:34:45 +0000

    Added author/contributor info

--- contracts/CloudStaking.sol
+++ contracts/CloudStaking.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import upgradeable contract modules from OpenZeppelin
+// Author: CloudAI Dev Team
+// Developed by @BullBoss5
+
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
--- contracts/CloudUtils.sol
+++ contracts/CloudUtils.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// Author: CloudAI Dev Team
+// Developed by @BullBoss5
+
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
