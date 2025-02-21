9a14d1f4d7305130eb283c26a725af0ac097f7f3
IO <alfamastudio11@gmail.com>
2025-02-21 17:14:21 +0000

    Reentrancy protection

--- contracts/CloudStakeVault.sol
+++ contracts/CloudStakeVault.sol
@@ -4,15 +4,16 @@ pragma solidity ^0.8.0;
 // Author: CloudAI Dev Team
 // Developed by @BullBoss5
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract CloudStakeVault is Ownable {
+contract CloudStakeVault is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable cloudToken;
-    address public cloudStaking;
+    address public cloudStakingContractAdress;
     uint256 public constant EMERGENCY_COOLDOWN = 30 days;
 
     mapping(address => uint256) private userDeposits;
@@ -28,12 +29,12 @@ contract CloudStakeVault is Ownable {
         require(_cloudToken   != address(0),    "Invalid token address");
         require(_cloudStaking != address(0),    "Invalid staking address");
 
-        cloudToken   = IERC20(_cloudToken);
-        cloudStaking = _cloudStaking;
+        cloudToken                   = IERC20(_cloudToken);
+        cloudStakingContractAdress   = _cloudStaking;
     }
 
     modifier onlyStakingContract() {
-        require(msg.sender == cloudStaking, "Only CloudStaking can call this function");
+        require(msg.sender == cloudStakingContractAdress, "Only CloudStaking can call this function");
         _;
     }
 
@@ -41,17 +42,17 @@ contract CloudStakeVault is Ownable {
     // PUBLIC FUNCTIONS
     // ============================================
 
-    function setStakingContractAddress(address _newCloudStaking)    external onlyOwner {
-        require(_newCloudStaking != address(0),      "Invalid address");
-        require(_newCloudStaking != cloudStaking,    "Same address already set");
+    function setStakingContractAddress(address _newCloudStaking)                        external onlyOwner {
+        require(_newCloudStaking != address(0),                     "Invalid address");
+        require(_newCloudStaking != cloudStakingContractAdress,     "Same address already set");
 
-        address oldCloudStaking = cloudStaking;
-        cloudStaking            = _newCloudStaking;
+        address oldCloudStaking               = cloudStakingContractAdress;
+        cloudStakingContractAdress            = _newCloudStaking;
 
-        emit StakingContractAddressUpdated(oldCloudStaking, cloudStaking);
+        emit StakingContractAddressUpdated(oldCloudStaking, cloudStakingContractAdress);
     }
 
-    function deposit(address user, uint256 amount)                  external onlyStakingContract {
+    function deposit(address user, uint256 amount)                                      external onlyStakingContract nonReentrant {
         require(user != address(0), "Invalid user address");
         require(amount > 0,         "Amount must be greater than zero");
 
@@ -63,7 +64,7 @@ contract CloudStakeVault is Ownable {
         emit Deposited(user, amount);
     }
 
-    function withdraw(address user, uint256 amount)                 external onlyStakingContract {
+    function withdraw(address user, uint256 amount)                                     external onlyStakingContract nonReentrant {
         require(user != address(0),             "Invalid user address");
         require(userDeposits[user] >= amount,   "Insufficient user balance");
 
@@ -75,7 +76,7 @@ contract CloudStakeVault is Ownable {
         emit Withdrawn(user, amount);
     }
 
-    function emergencyWithdraw()                                    external {
+    function emergencyWithdraw()                                                        external {
         require(userDeposits[msg.sender] > 0,                   "No funds to withdraw");
         require(emergencyWithdrawRequests[msg.sender] == 0,     "Already requested");
 
@@ -84,7 +85,7 @@ contract CloudStakeVault is Ownable {
         emit EmergencyWithdrawRequested(msg.sender, block.timestamp);
     }
 
-    function claimEmergencyWithdraw()                               external {
+    function claimEmergencyWithdraw()                                                   external nonReentrant {
         require(emergencyWithdrawRequests[msg.sender] > 0,                                      "No pending emergency withdrawal");
         require(block.timestamp >= emergencyWithdrawRequests[msg.sender] + EMERGENCY_COOLDOWN,  "Emergency cooldown not finished");
 
@@ -105,7 +106,7 @@ contract CloudStakeVault is Ownable {
         IERC20(_token).safeTransfer(_recipient, _amount);
     }
 
-    receive()                                                       external payable {
+    receive()                                                                           external payable {
         revert("Direct transfers not allowed");
     }
 
@@ -114,11 +115,8 @@ contract CloudStakeVault is Ownable {
     // ============================================
 
 
-    function getDepositedBalance(address user)                      external view returns (uint256) {
+    function getDepositedBalance(address user)                                          external view returns (uint256) {
         return userDeposits[user];
     }
 
-    function getStakingContractAddress()                            external view returns (address) {
-        return cloudStaking;
-    }
 }
